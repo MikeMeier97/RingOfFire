f39167dcd43390978ef89fd329bfb52aeae92366
MikeMeier97 <m0.0m58@yahoo.de>
2023-01-06 01:12:55 +0100

    stack addet

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from '../models/game';
 
 @Component({
   selector: 'app-game',
@@ -6,11 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit{
+  pickCardAnimation = false; 
+  game: Game = new Game; 
+  constructor(){
+    console.log(this.game);
+  }
   ngOnInit(): void {
     
   }
-  pickCardAnimation = false; 
   takeCard() {
     this.pickCardAnimation = true; 
+  }
+  newGame() {
+    this.game = new Game();
   }
 }
---
+++ src/app/models/game.ts
@@ -0,0 +1,27 @@
+export class Game {
+    public players: string[] = []; 
+    public stack: string[] = [];
+    public playedCards: string[] = [];
+    public currentPlayer: number = 0; 
+
+    constructor() {
+        for(let i = 0; i < 14; i++) {
+            this.stack.push('spade_'+ i);
+            this.stack.push('hearts_'+ i);
+            this.stack.push('clubs_'+ i);
+            this.stack.push('diamonds_'+ i);
+        }
+        shuffle(this.stack);
+    }
+}
+
+function shuffle(array: string[]) {
+    var currentIndex = array.length, temporaryValue, randomIndex; 
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1; 
+        temporaryValue = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array; 
+}
