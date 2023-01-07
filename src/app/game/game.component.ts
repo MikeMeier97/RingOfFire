1db9d0a37f6e9b957d964684ab8b4b96d1ea7add
Mike Meier <m0.0m58@yahoo.de>
2023-01-07 11:47:24 +0100

    player addet

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -4,21 +4,26 @@ import { Game } from '../models/game';
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
-export class GameComponent implements OnInit{
-  pickCardAnimation = false; 
-  game: Game = new Game; 
-  constructor(){
-    console.log(this.game);
-  }
-  ngOnInit(): void {
-    
-  }
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  game: Game = new Game();
+  currentCard: string = 'ace_1';
+  constructor() {}
+  ngOnInit(): void {}
   takeCard() {
-    this.pickCardAnimation = true; 
+    if (!this.pickCardAnimation) {
+      this.pickCardAnimation = true;
+      //this.currentCard = this.game.stack.pop();
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+        this.game.playedCards.push(this.currentCard);
+      }, 1000);
+    }
   }
   newGame() {
+    console.log(this.game.stack.pop);
     this.game = new Game();
   }
 }
